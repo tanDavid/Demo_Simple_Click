e15b52165680f6c2133fcc8a7627c014f1b15c43
TAN DAVID <19002765@myrp.edu.sg>
2020-05-08 16:00:11 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19002765/demosimpleclick/MainActivity.java
@@ -0,0 +1,54 @@
+package sg.edu.rp.c346.id19002765.demosimpleclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText eInput;
+    ToggleButton tbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay = findViewById(R.id.buttonDisplay);
+        eInput = findViewById(R.id.editTextDisplay);
+        tbtn = findViewById(R.id.toggleButtonEnabled);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringResponse = eInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+            }
+        });
+
+        tbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(tbtn.isChecked()){
+                    eInput.setEnabled(true);
+                }
+
+                else{
+                    eInput.setEnabled(false);
+                }
+
+            }
+        });
+
+
+    }
+}
